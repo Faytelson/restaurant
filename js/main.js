c11fe1d69ca452cd167fa749e483abcafd59b520
Faytelson <daryafaitelson@gmail.com>
2022-07-21 19:57:27 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,80 +1,80 @@
 "use strict";
+// window.addEventListener('DOMContentLoaded', function(){
 
-window.addEventListener("DOMContentLoaded", () => {
+
     //tabs
-
     (function() {
-        const tabParent = document.querySelector('.tabheader__items'),
-        tabs = document.querySelectorAll('.tabheader__item'),
-        contents = document.querySelectorAll('.tabcontent');
+        const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsParent = document.querySelector('.tabheader__items'),
+            contents = document.querySelectorAll('.tabcontent');
 
-        for(let i = 0; i < tabs.length; i ++) {
-            tabs[i].dataset.num = i;
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].dataset.open = i;
         }
 
-        tabParent.addEventListener('click', function(event){
-            if(event.target && event.target.classList.contains('tabheader__item')) {
-                contents.forEach(function(elem) {
-                    elem.classList.remove('tabcontent_active');
-                });  
-                tabs.forEach(function(elem){
-                    elem.classList.remove('tabheader__item_active');
-                })                    
-                event.target.classList.add('tabheader__item_active');
-                contents[event.target.dataset.num].classList.add('tabcontent_active');
+        tabsParent.addEventListener('click', function(event){
+            const target = event.target;
+            if(target && target.classList.contains('tabheader__item')) {
+                for(let i =0; i < tabs.length; i++) {
+                    tabs[i].classList.remove('tabheader__item_active');
+                    contents[i].classList.remove('tabcontent_active');
+                }
+                target.classList.add('tabheader__item_active');
+                contents[target.dataset.open].classList.add('tabcontent_active');
             }
-        });
+        })
     })();
 
+
+
+
     //timer
-
-
-    (function() {
-        const deadline = "2022-9-29";
+    (function(){
+        const deadline = '2022-09-01';
 
         function getTimeLeft(endTime) {
             let t = Date.parse(endTime) - Date.parse(new Date()),
                 days = Math.floor(t / (1000 * 60 * 60 * 24)),
                 hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-                mins = Math.floor((t / 1000 / 60) % 60),
-                secs = Math.floor((t / 1000) % 60);
-    
+                minutes = Math.floor((t / (1000 * 60)) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+                
                 return {
                     total: t,
                     days,
                     hours,
-                    mins,
-                    secs,
+                    minutes,
+                    seconds,
                 }
         }
-    
+
         function setTimer(selector, endTime) {
             let timer = document.querySelector(selector),
                 daysField = timer.querySelector('#days'),
                 hoursField = timer.querySelector('#hours'),
                 minsField = timer.querySelector('#minutes'),
                 secsField = timer.querySelector('#seconds'),
-                timeInterval = setInterval(updateTimer, 1000);
-    
+                timeInt = setInterval(updateTimer, 1000);
+
             updateTimer();
-                
+            
             function updateTimer() {
-                let {total, days, hours, mins, secs} = getTimeLeft(endTime);
-                daysField.innerHTML = addZero(days),
-                hoursField.innerHTML = addZero(hours),
-                minsField.innerHTML = addZero(mins),
-                secsField.innerHTML = addZero(secs);
-    
-                if(total <= 0) {
-                    daysField.innerHTML = addZero(0),
-                    hoursField.innerHTML = addZero(0),
-                    minsField.innerHTML = addZero(0),
+                let {days, hours, minutes, seconds, total} = getTimeLeft(endTime);
+                daysField.innerHTML = addZero(days);
+                hoursField.innerHTML = addZero(hours);
+                minsField.innerHTML = addZero(minutes);
+                secsField.innerHTML = addZero(seconds);
+
+                if(total && total <= 0) {
+                    daysField.innerHTML = addZero(0);
+                    hoursField.innerHTML = addZero(0);
+                    minsField.innerHTML = addZero(0);
                     secsField.innerHTML = addZero(0);
-    
-                    clearInterval(timeInterval);
+
+                    clearInterval(timeInt);
                 }
             }
-    
+
             function addZero(num) {
                 if(num >= 0 && num < 10) {
                     return `0${num}`;
@@ -84,56 +84,52 @@ window.addEventListener("DOMContentLoaded", () => {
         setTimer('.timer', deadline);
     })();
 
+
+
     //modal
 
-    (function(){
-        let btns = document.querySelectorAll('[data-modal-open]'),
+    (function() {
+        const btns = document.querySelectorAll('[data-modal-open'),
         modal = document.querySelector('.modal'),
-        btnCloseModal = document.querySelector('[data-modal-close]'),
-        timeInterval = setTimeout(showModal, 15000);
+        btnCloseModal = document.querySelector('[data-modal-close'),
+        timeInt = setTimeout(showModal, 5000);
 
         function showModal() {
             modal.style.display = 'block';
             document.body.style.overflow = 'hidden';
-            clearInterval(timeInterval);
-            window.removeEventListener('scroll', checkModal);
+            clearInterval(timeInt);
+            window.removeEventListener('scroll', showModalByScroll);
         }
-        btns.forEach(function(btn){
-            btn.addEventListener('click', showModal);
-        });
 
         function closeModal() {
             modal.style.display = 'none';
             document.body.style.overflow = '';
         }
-        btnCloseModal.addEventListener('click', closeModal)
 
-        modal.addEventListener('click', function(event) {
-            if(event.target === modal) {
-                closeModal();
-            }
-        });
+    btns.forEach((btn) => {
+        btn.addEventListener('click', showModal)
+    });
 
-        document.addEventListener('keydown', (event) => {
-            if(event.code === 'Escape' && modal.style.display == 'block') {
-                closeModal();
-            }
-        })
+    btnCloseModal.addEventListener('click', closeModal);
 
-        function checkModal(){
-            if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-                showModal();
-                // this.removeEventListener('scroll', checkModal);
-            }
+    modal.addEventListener('click', (event) => {
+        const target = event.target;
+        if(target === modal) {
+            closeModal();
         }
+    })
 
-        window.addEventListener('scroll', checkModal);
-        // window.addEventListener('scroll', function checkModal(event){
-        //     if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-        //         showModal();
-        //         this.removeEventListener('scroll', checkModal);
-        //     }
-        // })
+    window.addEventListener('keydown', (event) => {
+        if(event.code === 'Escape' && modal.style.display === 'block') {
+            closeModal();
+        }
+    })
+    function showModalByScroll() {
+        if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            showModal();
+        }
+    }
+    window.addEventListener('scroll', showModalByScroll);
     })();
 
-});
+// });
